35622fad7c625fd929d34f7e62f39fdf295d261e
Ramesh Vagadiya <vagadiya@gmail.com>
2019-12-29 22:58:24 +0000

    Add CandleChart constants

---
+++ ig-webapi-java-client/src/main/java/com/iggroup/webapi/samples/client/CandleChart.java
@@ -0,0 +1,21 @@
+package com.iggroup.webapi.samples.client;
+
+public class CandleChart {
+    public static final String DAY_PERC_CHG_MID = "DAY_PERC_CHG_MID";
+    public static final String LTV = "LTV";
+    public static final String UTM = "UTM";
+    public static final String DAY_OPEN_MID = "DAY_OPEN_MID";
+    public static final String DAY_NET_CHG_MID = "DAY_NET_CHG_MID";
+    public static final String DAY_HIGH = "DAY_HIGH";
+    public static final String DAY_LOW = "DAY_LOW";
+    public static final String OFR_OPEN = "OFR_OPEN";
+    public static final String OFR_HIGH = "OFR_HIGH";
+    public static final String OFR_LOW = "OFR_LOW";
+    public static final String OFR_CLOSE = "OFR_CLOSE";
+    public static final String BID_OPEN = "BID_OPEN";
+    public static final String BID_HIGH = "BID_HIGH";
+    public static final String BID_LOW = "BID_LOW";
+    public static final String BID_CLOSE = "BID_CLOSE";
+    public static final String CANDLE_START = "CANDLE_START";
+    public static final String CANDLE_TICK_COUNT = "CANDLE_TICK_COUNT";
+}
